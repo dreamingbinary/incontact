9622c8198392e9b8d5e9f2c22fb6497b9bc5146d
Brian Tennity <btennity+git@customink.com>
2017-04-18 16:24:18 -0400

    Update Connection class
    Will now be subclassed by other resource classes

--- lib/incontact_client/connection.rb
+++ lib/incontact_client/connection.rb
@@ -5,76 +5,109 @@ module InContactClient
     MAX_TRIES    = 2
     RETRY_SLEEP  = 1
 
-    attr_reader :url, :authorization
+    class << self
+      def get(path, params = {})
+        request :get, path, params
+      end
 
-    def initialize(url, authorization)
-      @url           = url
-      @authorization = authorization
-    end
+      def post(path, payload, params = {})
+        request :post, path, params, payload
+      end
 
-    def get(path, params = {})
-      request :get, path, params
-    end
-
-    def post(path, payload, params = {})
-      request :post, path, params, payload
-    end
-
-    def request(type, path, params = {}, body=nil)
-      timeout_override      = params.delete :timeout
-      open_timeout_override = params.delete :open_timeout
-      response              = Retryable.retryable(retryable_options(params)) do
-        request_with_exception_handling do
-          connection.send(type) do |req|
-            req.path                   = path
-            req.options[:timeout]      = timeout_override      || TIMEOUT
-            req.options[:open_timeout] = open_timeout_override || OPEN_TIMEOUT
-            req.params                 = params
-            req.body                   = body.to_json if body
+      def request(type, path, params = {}, body=nil)
+        timeout_override      = params.delete :timeout
+        open_timeout_override = params.delete :open_timeout
+        data_model_override   = params.delete :data_model_override
+        response              = Retryable.retryable(retryable_options(params)) do
+          request_with_exception_handling do
+            connection.send(type) do |req|
+              req.path                   = path
+              req.options[:timeout]      = timeout_override      || TIMEOUT
+              req.options[:open_timeout] = open_timeout_override || OPEN_TIMEOUT
+              req.params                 = params
+              req.body                   = body.to_json if body
+            end
           end
         end
+        raise_response_errors(response)
+        parse_response(response, data_model_override)
       end
-      raise_response_errors(response)
-      response.body
-    end
 
-    private
-
-    def retryable_options(options)
-      {}.tap do |retry_options|
-        retry_options[:tries] = options.delete(:max_tries)   || MAX_TRIES
-        retry_options[:sleep] = options.delete(:retry_sleep) || RETRY_SLEEP
-        retry_options[:on]    = [ConnectionError]
+      def connection
+        Thread.current[:incontact_client_connection] ||= create_new_connection
       end
-    end
 
-    def request_with_exception_handling
-      yield
-    rescue => exception
-      raise ConnectionError, exception
-    end
-
-    def raise_response_errors(response)
-      return if response.success?
-      error_messages = response.body.respond_to?(:exception_message) &&
-        response.body.errors.present? ? response.body.errors.join("\n") : ""
-      case response.status
-      when 404
-        raise ResourceNotFound.new(error_messages, response)
-      when 400...600
-        raise Error.new(error_messages, response)
+      def reestablish_connection
+        Thread.current[:incontact_client_connection] = nil
       end
-    end
 
-    def connection
-      @connection ||= Faraday.new do |faraday|
-        faraday.url_prefix                 = url
-        faraday.headers["Accept-Encoding"] = "none"
-        faraday.headers["Content-Type"]    = "application/json; charset=utf-8"
-        faraday.headers["Authorization"]   = authorization
-        faraday.use FaradayMiddleware::Mashify
-        faraday.use FaradayMiddleware::ParseJson
-        faraday.adapter Faraday.default_adapter
+      private
+
+      def retryable_options(options)
+        {}.tap do |retry_options|
+          retry_options[:tries] = options.delete(:max_tries)   || MAX_TRIES
+          retry_options[:sleep] = options.delete(:retry_sleep) || RETRY_SLEEP
+          retry_options[:on]    = [ConnectionError]
+        end
+      end
+
+      def request_with_exception_handling
+        yield
+      rescue => exception
+        raise ConnectionError, exception
+      end
+
+      def raise_response_errors(response)
+        return if response.success?
+        error_messages = response.body.respond_to?(:exception_message) &&
+          response.body.errors.present? ? response.body.errors.join("\n") : ""
+        case response.status
+        when 404
+          raise ResourceNotFound.new(error_messages, response)
+        when 400...600
+          raise Error.new(error_messages, response)
+        end
+      end
+
+      def create_new_connection
+        Faraday.new do |faraday|
+          faraday.url_prefix                 = url
+          faraday.headers["Accept-Encoding"] = "none"
+          faraday.headers["Content-Type"]    = "application/json; charset=utf-8"
+          faraday.headers["Authorization"]   = authorization
+          faraday.use FaradayMiddleware::Mashify
+          faraday.use FaradayMiddleware::ParseJson
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      def parse_response(response, data_model_override)
+        data = response.body
+        if data.is_a?(Array)
+          data.map { |item| map_response_item_to_model(item, data_model_override) }
+        else
+          map_response_item_to_model(data, data_model_override)
+        end
+      end
+
+      def data_model(item)
+        klass = item && item._type ? item._type.classify : name.demodulize.underscore.singularize.classify
+        "InContactClient::Models::#{klass}".constantize rescue nil
+      end
+
+      def map_response_item_to_model(item, data_model_override)
+        return item if item.blank? || data_model_override.is_a?(FalseClass)
+        data_model_klass = data_model_override || data_model(item)
+        return item if data_model_klass.nil?
+        data_model_klass.new(item)
+      end
+
+      def url
+        raise NotImplementedError
+      end
+
+      def authorization
+        raise NotImplementedError
       end
     end
   end
